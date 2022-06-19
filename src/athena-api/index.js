0a3e8a518f9ea85b05403f94cea5e176d71d954d
tharangathennakoon <tharanganilupul@gmail.com>
2022-06-19 18:34:53 -0500

    cd

--- src/athena-api/index.js
+++ src/athena-api/index.js
@@ -9,8 +9,10 @@ const {
     preparePaginationRequest
 } = require("common-lib/utils/pagination-utils");
 const { InvalidError } = require('common-lib/exception');
-
+const { AthenaClient, StartQueryExecutionCommand }  = require("@aws-sdk/client-athena"); //
 const RESOURCE_PATH = '/athena';
+
+const athenaClient = new AthenaClient();
 
 exports.handler = async function (event, context) {
     console.log("event", JSON.stringify(event));
@@ -19,8 +21,18 @@ exports.handler = async function (event, context) {
         const httpMethod = event.httpMethod;
 
         if (resourcePath === RESOURCE_PATH && httpMethod === "GET") {
-
-            return sendResponse({message: '1'});
+            const command = new StartQueryExecutionCommand({
+                "QueryString": "SELECT * FROM testdb.dim_answer;",
+                QueryExecutionContext: {
+                    Catalog: "target_db_demo_2",
+                    Database: "testdb"
+                },
+                ResultConfiguration: {
+                    OutputLocation: "s3://athena-demo-test-1/query-results"
+                }
+            });
+            const response = await athenaClient.send(command);
+            return sendResponse(response);
 
         } else {
             throw new InvalidError(`invalid route ${resourcePath}:${httpMethod}`);
